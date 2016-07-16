607052c2c54b67a1deff371a950f6a6933ae6dcf
pratik.ac <pratik.ac@gmail.com>
2016-07-16 23:58:54 +0800

    added viewer

--- src/viewer/viewer.cpp
+++ src/viewer/viewer.cpp
@@ -3,26 +3,87 @@
 
 #include <gtk/gtk.h>
 
+#include <lcm/lcm.h>
+#include <bot_core/bot_core.h>
 #include <bot_vis/bot_vis.h>
+#include <bot_lcmgl_render/lcmgl_bot_renderer.h>
+#include "udp_util.h"
+#include "view_menu.h"
 
+static int
+logplayer_remote_on_key_press(BotViewer *viewer, BotEventHandler *ehandler,
+        const GdkEventKey *event)
+{
+    int keyval = event->keyval;
+
+    switch (keyval)
+    {
+        case 'P':
+        case 'p':
+            udp_send_string("127.0.0.1", 53261, "PLAYPAUSETOGGLE");
+            break;
+        case 'N':
+        case 'n':
+            udp_send_string("127.0.0.1", 53261, "STEP");
+            break;
+        case '=':
+        case '+':
+            udp_send_string("127.0.0.1", 53261, "FASTER");
+            break;
+        case '_':
+        case '-':
+            udp_send_string("127.0.0.1", 53261, "SLOWER");
+            break;
+        case '[':
+            udp_send_string("127.0.0.1", 53261, "BACK5");
+            break;
+        case ']':
+            udp_send_string("127.0.0.1", 53261, "FORWARD5");
+            break;
+        default:
+            return 0;
+    }
+
+    return 1;
+}
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
     glutInit(&argc, argv);
-    g_thread_init(0);
-    
+    setlinebuf(stdout);
+
+    lcm_t* lcm = lcm_create(NULL);
+
     BotViewer* viewer = bot_viewer_new("corvis-viewer");
+    bot_glib_mainloop_attach_lcm(lcm);
+    setup_view_menu(viewer);
 
     char *fname = g_build_filename(g_get_user_config_dir(), ".corvis-viewerrc", NULL);
-    
-    bot_viewer_load_preferences(viewer, fname);
 
     // setup renderers
     bot_viewer_add_stock_renderer(viewer, BOT_VIEWER_STOCK_RENDERER_GRID, 1);
+    bot_lcmgl_add_renderer_to_viewer(viewer, lcm, 0);
 
+    // logplayer controls
+    BotEventHandler *ehandler = (BotEventHandler*) calloc(1, sizeof(BotEventHandler));
+    ehandler->name = (char*)"LogPlayer Remote";
+    ehandler->enabled = 1;
+    ehandler->key_press = logplayer_remote_on_key_press;
+    bot_viewer_add_event_handler(viewer, ehandler, 0);
+
+    glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+    glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
+
+    bot_viewer_load_preferences(viewer, fname);
     gtk_main();
 
     bot_viewer_save_preferences(viewer, fname);
+    free(fname);
 
     bot_viewer_unref(viewer);
+    bot_glib_mainloop_detach_lcm(lcm);
+    lcm_destroy(lcm);
+
+    return 0;
 }
